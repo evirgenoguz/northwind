1d2442646db7e7fbc111ea4e280c969ca35920bc
Oğuz Evirgen <59138609+evirgenoguz@users.noreply.github.com>
2021-04-05 16:08:25 +0300

    ders tekrarı

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -13,7 +13,13 @@ export class ProductComponent implements OnInit {
   product4: any = { productId: 4, productName: 'Keyboard', categoryId: 1, unitPrice: 5 };
   product5: any = { productId: 5, productName: 'Camera', categoryId: 1, unitPrice: 5 };
 
-  products = [this.product1, this.product2, this.product3, this.product4, this.product5];
+  products = [
+    this.product1, 
+    this.product2, 
+    this.product3, 
+    this.product4, 
+    this.product5,
+  ];
   constructor() { }
 
   ngOnInit(): void {
