41bd156b488248041d87d0c025cb74263fdfd22f
Oğuz Evirgen <59138609+evirgenoguz@users.noreply.github.com>
2021-04-16 11:33:30 +0300

    20. Ders Ara

--- src/app/components/product-add/product-add.component.ts
+++ src/app/components/product-add/product-add.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms'
+import { ToastrService } from 'ngx-toastr';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-add',
@@ -10,7 +12,7 @@ export class ProductAddComponent implements OnInit {
 
   productAddForm : FormGroup;
 
-  constructor(private formBuilder : FormBuilder) { }
+  constructor(private formBuilder : FormBuilder, private productService : ProductService, private toastrService : ToastrService) { }
 
   ngOnInit(): void {
     this.createProductAddForm();
@@ -23,5 +25,23 @@ export class ProductAddComponent implements OnInit {
       unitsInStock : ["", Validators.required],
       categoryId : ["", Validators.required]
     })
+  }
+
+  add(){
+    if (this.productAddForm.valid) {
+      let productModel = Object.assign({}, this.productAddForm.value)
+      this.productService.add(productModel).subscribe(response => {
+        console.log(response);
+        this.toastrService.success(response.message, "Başarılı");
+      }, responseError => {
+        console.log(responseError)
+        this.toastrService.error(responseError.error)
+      });
+    }
+    else{
+      this.toastrService.error("Ürün Eklenemedi", "Kontrol Ediniz");
+    }
+
+    
   }
 }
